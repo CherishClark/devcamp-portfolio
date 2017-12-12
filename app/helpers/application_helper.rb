85b45d784f1ffdc799a2dd85f837ca03591b5db0
Cherish Clark <cherishclark@cherishs-mbp.ad.uvu.edu>
2017-12-12 15:06:40 -0700

    added styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User) 
-      link_to "logout",  destroy_user_session_path, method: :delete
-      else 
-      (link_to "Register",  new_user_registration_path) +
+    if current_user.is_a?(GuestUser) 
+        (link_to "Register",  new_user_registration_path) +
       "<br>".html_safe +
       (link_to "Login", new_user_session_path)
-      end 
-    end
+    else 
+      link_to "logout",  destroy_user_session_path, method: :delete
+    end 
+  end
   def source_helper(layout_name)
     if session[:source] 
       greeting = "Thanks for visitiong me from #{session[:source]} and you are on the #{layout_name} layout"
