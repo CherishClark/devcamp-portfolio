12ea0a6fe866cbbf0b19532b4408cb65aca6c625
CherishClark <cherish.g.clark@gmail.com>
2017-12-19 12:11:59 -0700

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,11 +50,22 @@ module ApplicationHelper
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}'' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
-
   nav_links.html_safe
- end
+  end
 
  def active? path
   "active" if current_page? path
+ end
+
+ def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice]) 
+
+  if alert
+    alert_generator alert
+  end  
+ end
+
+ def alert_generator msg
+  js add_gritter(msg, :title => "Cherish Clark's Portfolio", sticky: false)
  end
 end
